32b0cce9388d3a5680617ff01aee92a44764b613
jhenioliver <jhenifferoliveirafoschera@gmail.com>
2023-08-10 15:53:57 -0300

    ajustes

--- listacompleta.php
+++ listacompleta.php
@@ -27,12 +27,12 @@
                                     U.NOME AS NOME,
                                     U.SOBRENOME AS SOBRENOME,
                                     CONCAT(UCASE(SUBSTRING(D.NOME_DEPARTAMENTO, 1, 1)), LCASE(SUBSTRING(D.NOME_DEPARTAMENTO, 2))) AS SETOR, 
-                                    DATE_FORMAT(U.DATA_NASCIMENTO, '%d-%m') AS DATA
+                                    DATE_FORMAT(U.DATA_NASCIMENTO, '%d/%m') AS DATA
                                 FROM USUARIO U
                                     JOIN DEPARTAMENTO D
                                         ON D.CD_DEPARTAMENTO = U.CD_DEPARTAMENTO
                                 WHERE MONTH(U.DATA_NASCIMENTO) = MONTH(NOW())
-                                ORDER BY U.NOME ASC";
+                                ORDER BY DATA ASC";
 
         $sql_query_aniversario = $conn->query($sql_code_aniversario) or die ("ERRO ao consultar! " . $conn->error);
         while($dados = $sql_query_aniversario->fetch_assoc()) { ?>
